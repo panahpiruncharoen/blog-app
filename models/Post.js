b4e3a2272da40b693ec3ab5c53c6f3577ac227cb
panahpiruncharoen <panahpirun@gmail.com>
2025-03-17 11:44:40 +0000

    add numLikes to Post model

--- models/Post.js
+++ models/Post.js
@@ -4,6 +4,7 @@ const postSchema = new mongoose.Schema({
     title: String,
 	author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     text: String,
+	numLikes: Number,
 }, { collection: 'posts' }); // Explicitly setting the collection name
 
 const Post = mongoose.model("Post", postSchema);
