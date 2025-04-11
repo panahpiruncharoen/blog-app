e619dc3b2ee6ef3edc364f8e40442292a8ca0d2c
panahpiruncharoen <panahpirun@gmail.com>
2025-04-11 10:33:00 +0000

    create profile routes and page

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const postRoutes = require('./routes/postRoutes')
 const commentRoutes = require('./routes/commentRoutes')
 const userRoutes = require('./routes/userRoutes')
 const authRoutes = require('./routes/authRoutes')
+
+const User = require("./models/User")
 // const userRoutes = require('routes/userRoutes')
 
 const app = express()
@@ -46,6 +48,34 @@ app.use("/comments", commentRoutes)
 app.use("/users", userRoutes)
 app.use("/auth", authRoutes)
 
+app.get("/profile", (req, res) => {
+	if (!req.user) {
+	  res.send("You are not logged in")
+	}
+	
+	res.render("profile/profile")
+})
+
+app.patch("/profile", async (req, res) => {
+	if (!req.user) {
+	  res.send("You are not logged in")
+	}
+	//console.log("try to update profile")
+	//console.log(req.body)
+	//console.log(req.user.id)
+	
+	const newUsername = req.body.username
+	const newFirstName = req.body.firstName
+	const newLastName = req.body.lastName
+	const newEmail= req.body.email
+	
+	await User.findByIdAndUpdate(
+		req.user.id,
+		{$set: {username: newUsername, firstName: newFirstName, lastName: newLastName, email: newEmail}}
+	)
+	
+	res.redirect("/profile")
+})
 // app.use("/users", userRoutes)
 
 app.listen(3000, () => {
