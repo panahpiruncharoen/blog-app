7571c9a5ac1d2f0027b38c206f7f9fecaae5092c
panahpiruncharoen <panahpirun@gmail.com>
2025-04-17 10:28:01 +0000

    create userController.js and refactor code from userRoutes.js

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const User = require('../models/User')
+const Post = require("../models/Post")
+
+exports.getUserProfile = async (req, res) => {
+	if (!req.user) {
+	  res.send("You are not logged in")
+}
+	const userPosts =  await Post.find({ author: req.user })
+	console.log(userPosts)
+	res.render("users/profile", { userPosts })
+}
+
+exports.updateUserProfile = async (req, res) => {
+if (!req.user) {
+	  res.send("You are not logged in")
+	}
+	//console.log("try to update profile")
+	//console.log(req.body)
+	//console.log(req.user.id)
+	
+	const newUsername = req.body.username
+	const newFirstName = req.body.firstName
+	const newLastName = req.body.lastName
+	const newEmail= req.body.email
+	
+	await User.findByIdAndUpdate(
+		req.user.id,
+		{$set: {username: newUsername, firstName: newFirstName, lastName: newLastName, email: newEmail}}
+	)
+	
+	res.redirect("/users/profile")
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,39 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
-const User = require('../models/User')
-const Post = require("../models/Post")
+const userController = require('../controllers/userController.js')
 
-router.get("/profile", async(req, res) => {
-	if (!req.user) {
-	  res.send("You are not logged in")
-	}
-	
-	const userPosts =  await Post.find({ author: req.user })
-	console.log(userPosts)
-	res.render("users/profile", { userPosts })
-})
+router.get("/profile", userController.getUserProfile)
 
-router.patch("/profile", async (req, res) => {
-	if (!req.user) {
-	  res.send("You are not logged in")
-	}
-	//console.log("try to update profile")
-	//console.log(req.body)
-	//console.log(req.user.id)
-	
-	const newUsername = req.body.username
-	const newFirstName = req.body.firstName
-	const newLastName = req.body.lastName
-	const newEmail= req.body.email
-	
-	await User.findByIdAndUpdate(
-		req.user.id,
-		{$set: {username: newUsername, firstName: newFirstName, lastName: newLastName, email: newEmail}}
-	)
-	
-	res.redirect("/users/profile")
-})
+router.patch("/profile", userController.updateUserProfile)
 
 //register
 
